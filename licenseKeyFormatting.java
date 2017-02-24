71f1fed334e0b9e4e0ab0b10eb4ff6e74d7726f9
meow730 <meow730730@gmail.com>
2017-02-24 14:44:51 -0800

    add solu for 482

---
+++ licenseKeyFormatting.java
@@ -0,0 +1,11 @@
+public String licenseKeyFormatting(String s, int k) {
+  StringBuilder sb = new StringBuilder();
+   // A clever way
+  for (int i = s.length() - 1; i >= 0; i--) {
+    if (s.charAt(i) != '-') {
+      sb.append(sb.length() % (k + 1) == k ? '_' : "");
+      sb.append(s.charAt(i));
+    }
+    return sb.reverse().toString().toUpperCase();
+  }
+}
