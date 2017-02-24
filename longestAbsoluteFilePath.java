8ad1bf7e33ce75b3e0f794696cf11a4c727505ff
meow730 <meow730730@gmail.com>
2017-02-24 14:19:38 -0800

    add solu for 388

--- longestAbsoluteFilePath.java
+++ longestAbsoluteFilePath.java
@@ -1,3 +1,15 @@
 public int lengthLongestPath(String input) {
-  // test
+  int max = 0;
+  HashMap<Integer, Integer> map = new HashMap<>();// level, current length
+  String[] parts = input.split("\n");
+  for (String p : parts) {
+    int level = p.lastIndexOf("\t") + 1; // \t, \n counts as 1
+    int parent = map.contains(level - 1) ? map.get(level - 1) : 0;
+    int len = level.length() - level + parent + 1; // assume dir
+    map.put(level, len);
+    if (part.contains(".")) {
+      max = Math.max(max, len - 1);
+    }
+  }
+  return max;
 }
