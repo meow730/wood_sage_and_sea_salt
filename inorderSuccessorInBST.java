48a97857c2c203f0fac94005c0d201f943a81820
meow730 <meow730730@gmail.com>
2017-03-20 21:09:02 -0700

    add 285

---
+++ inorderSuccessorInBST.java
@@ -0,0 +1,12 @@
+public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+  TreeNode suc = null;
+  while (root != null) {
+    if (root.val > p.val) {
+      suc =  root;
+      root = root.left;
+    } else {
+      root = root.right;
+    }
+  }
+  return suc;
+}
