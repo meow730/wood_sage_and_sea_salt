701f7b727b6188d96453116117b00569f46f01c2
meow730 <meow730730@gmail.com>
2017-03-20 22:01:51 -0700

    add 161

---
+++ editOneDistance.java
@@ -0,0 +1,16 @@
+public boolean isOneEditDistance(String s, String t) {
+  if (s.equals(t)) return false;
+  if (Math.abs(s.length() - t.length()) > 1) return false;
+  for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+    if (s.charAt(i) != t.charAt(i)) {
+      if (s.length() == t.length()) {
+        return s.substring(i + 1).equals(t.substring(i + 1));
+      } else if (s.length() < t.length()) {
+        return s.substring(i).equals(t.substring(i + 1));
+      } else {
+        return t.substring(i).equals(s.substring(i + 1));
+      }
+    }
+  } 
+  return true;
+}
